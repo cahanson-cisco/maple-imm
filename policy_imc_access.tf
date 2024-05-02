ed876a98323b9b9cf2fa4080491d57828fe72f99
Casey Hanson <cahanson@cisco.com>
2024-05-02 13:47:27 -0400

    adding imc access policy for DNS

--- policy_imc_access.tf
+++ policy_imc_access.tf
@@ -22,3 +22,28 @@ resource "intersight_access_policy" "inband_imc" {
     moid = intersight_ippool_pool.ip_pools["kvm_pool_mgmt"].id
   }
 }
+
+resource "intersight_access_policy" "inband_imc_dns" {
+  name = "inband_imc_dns"
+  dynamic "tags" {
+    for_each = local.tags
+    content {
+      key   = tags.key
+      value = tags.value
+    }
+  }
+  organization {
+    moid = local.organization
+  }
+
+  configuration_type {
+    configure_inband      = true
+    configure_out_of_band = false
+  }
+
+  inband_vlan = 850
+
+  inband_ip_pool {
+    moid = intersight_ippool_pool.ip_pools["kvm_pool_dns"].id
+  }
+}
