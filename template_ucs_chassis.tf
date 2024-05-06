e5e89a83064674ae048978efbf46b79b42b22a4c
Casey Hanson <cahanson@cisco.com>
2024-05-06 15:44:15 -0400

    adding ucs chassis template

---
+++ template_ucs_chassis.tf
@@ -0,0 +1,32 @@
+resource "intersight_chassis_profile" "chassis-template" {
+  name = "chassis-template"
+  dynamic "tags" {
+    for_each = local.tags
+    content {
+      key   = tags.key
+      value = tags.value
+    }
+  }
+  organization {
+    moid = local.organization
+  }
+
+  target_platform = "FIAttached"
+  type            = "instance"
+
+  policy_bucket { # IMC Access
+    moid        = intersight_access_policy.inband_imc.moid
+    object_type = intersight_access_policy.inband_imc.object_type
+  }
+
+  policy_bucket { # Power
+    moid        = intersight_power_policy.grid_last_state.moid
+    object_type = intersight_power_policy.grid_last_state.object_type
+  }
+
+  policy_bucket { # Thermal
+    moid        = intersight_thermal_policy.thermal["Balanced"].moid
+    object_type = intersight_thermal_policy.thermal["Balanced"].object_type
+  }
+
+}
